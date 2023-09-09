16a6e255a35f3687bd3e988241ba88cfc8181d66
Andrew Fitzgerald <apfitzge@gmail.com>
2023-09-09 10:30:37 -0700

    impl ResourceKey for any type impl the required traits (#10)

--- src/resource_key.rs
+++ src/resource_key.rs
@@ -2,3 +2,5 @@ use std::hash::Hash;
 
 /// A unique identifier that can identify resources used by a transaction.
 pub trait ResourceKey: Copy + Eq + Hash {}
+
+impl<T: Copy + Eq + Hash> ResourceKey for T {}
