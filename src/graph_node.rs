648c45d66179d4b247fcede542e68b12a40d3436
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-02 15:51:07 -0700

    Refactor: Separate graph_node.rs

---
+++ src/graph_node.rs
@@ -0,0 +1,14 @@
+use {crate::PriorityId, std::collections::HashSet};
+
+/// A node in the priority graph.
+pub struct GraphNode<Id: PriorityId> {
+    /// Unique edges from this node.
+    /// The number of edges is the same as the number of forks.
+    pub edges: HashSet<Id>,
+    /// Cache the reward for this node.
+    pub reward: u64,
+    /// Reward at next level - i.e. how much is blocked by this.
+    pub next_level_rewards: u64,
+    /// Number of edges into this node.
+    pub blocked_by_count: usize,
+}
