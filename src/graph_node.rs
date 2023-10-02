93ed01753177d3405cdb2c3a60338a43b72d789e
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-02 16:59:19 -0700

    Chain tracking (#31)

--- src/graph_node.rs
+++ src/graph_node.rs
@@ -2,6 +2,7 @@ use {crate::PriorityId, std::collections::HashSet};
 
 /// A node in the priority graph.
 pub struct GraphNode<Id: PriorityId> {
+    pub(crate) active: bool,
     /// Unique edges from this node.
     /// The number of edges is the same as the number of forks.
     pub edges: HashSet<Id>,
@@ -11,4 +12,6 @@ pub struct GraphNode<Id: PriorityId> {
     pub next_level_rewards: u64,
     /// Number of edges into this node.
     pub blocked_by_count: usize,
+    /// The distinct chain id of this node.
+    pub chain_id: u64,
 }
