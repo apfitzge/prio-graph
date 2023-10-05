98495b4d8daeeed7d6477f96a499a7b54069bdc4
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-05 16:14:47 -0700

    Docs: LockKind (#40)

--- src/lock_kind.rs
+++ src/lock_kind.rs
@@ -1,7 +1,7 @@
 use crate::PriorityId;
 
 /// A read-lock can be held by multiple transactions, and
-/// sub-sequent write-locks should be blocked by all of them.
+/// subsequent write-locks should be blocked by all of them.
 /// Write-locks are exclusive.
 pub(crate) enum LockKind<Id: PriorityId> {
     Read(Vec<Id>),
@@ -31,7 +31,7 @@ impl<Id: PriorityId> LockKind<Id> {
     pub fn add_write(&mut self, id: Id) -> Option<Vec<Id>> {
         match core::mem::replace(self, LockKind::Write(id)) {
             LockKind::Read(ids) => Some(ids),
-            LockKind::Write(id) => Some(vec![id]), // TODO: Remove allocation.
+            LockKind::Write(id) => Some(vec![id]),
         }
     }
 }
