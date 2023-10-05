afcaca47d54cf9c0c1e44605926a612357263551
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-05 15:38:54 -0700

    Docs: GraphNode

--- src/graph_node.rs
+++ src/graph_node.rs
@@ -1,13 +1,17 @@
 use {crate::PriorityId, std::collections::HashSet};
 
-/// A node in the priority graph.
+/// A single node in a priority graph that is associated with a `Transaction`.
+/// When a node reaches the main queue, the top-level prioritization function can use a reference
+/// to this node to calculate a modified priority for the node, which is only used for
+/// prioritization for the main queue.
 pub struct GraphNode<Id: PriorityId> {
+    /// Whether this node is active, i.e. has not been removed from the main queue.
     pub(crate) active: bool,
+    /// Number of edges into this node.
+    pub(crate) blocked_by_count: usize,
     /// Unique edges from this node.
     /// The number of edges is the same as the number of forks.
     pub edges: HashSet<Id>,
-    /// Number of edges into this node.
-    pub blocked_by_count: usize,
     /// The distinct chain id of this node.
     pub chain_id: u64,
 }
