7390ddc2eeb2030cb9340c5954d0dfe352336b0d
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-05 16:14:04 -0700

    Docs: Transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,10 +1,18 @@
 use crate::ResourceKey;
 
+/// Type of access for a specific resource key.
 pub enum AccessKind {
     Read,
     Write,
 }
 
+/// A transaction will access a set of resource keys with various `AccessKind`s.
 pub trait Transaction<Rk: ResourceKey> {
+    // The interface is a bit awkward, as rust does not currently support something like:
+    // ```rust
+    // fn check_resource_keys(&self) -> impl Iterator<Item = (&Rk, AccessKind)>;
+    // ```
+    // See: https://github.com/rust-lang/rust/issues/91611
+    /// Used by the `PrioGraph` to check and track conflicts in resource usage.
     fn check_resource_keys<F: FnMut(&Rk, AccessKind)>(&self, checker: F);
 }
