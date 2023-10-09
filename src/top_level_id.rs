712d1ef422015c99cbdfc5af6ff2370d3f03e292
Andrew Fitzgerald <apfitzge@gmail.com>
2023-10-09 11:11:56 -0700

    Fix comment on TopLevelId

--- src/top_level_id.rs
+++ src/top_level_id.rs
@@ -1,6 +1,6 @@
 use crate::TransactionId;
 
-/// A top-level ID that should contain the unique `Id`, but can be priority ordered differently.
+/// A top-level ID that should contain the unique `Id`, but can be priority ordered.
 pub trait TopLevelId<Id: TransactionId>: Eq + PartialEq + Ord + PartialOrd {
     fn id(&self) -> Id;
 }
