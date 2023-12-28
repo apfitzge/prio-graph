3d15bd66fa09d42569cdb926ae38c5674aeb3159
Andrew Fitzgerald <apfitzge@gmail.com>
2023-12-28 09:54:52 -0800

    Bugfix: Reads track most recent write (#57)

--- src/lock.rs
+++ src/lock.rs
@@ -4,7 +4,7 @@ use crate::TransactionId;
 /// subsequent write-locks should be blocked by all of them.
 /// Write-locks are exclusive.
 pub(crate) enum Lock<Id: TransactionId> {
-    Read(Vec<Id>),
+    Read(Vec<Id>, Option<Id>), // (Current Reads, Most Recent Write)
     Write(Id),
 }
 
@@ -13,17 +13,20 @@ impl<Id: TransactionId> Lock<Id> {
     /// Returns the id of the write transaction that is blocking the added read.
     pub fn add_read(&mut self, id: Id) -> Option<Id> {
         match self {
-            Lock::Read(ids) => {
+            Lock::Read(ids, maybe_write) => {
                 ids.push(id);
-                None
+                *maybe_write
             }
             Lock::Write(current_write_id) => {
                 // If the current write is the same as the one we're adding,
                 // do not overwrite the write-lock.
-                if *current_write_id == id {
+                let current_write_id = *current_write_id;
+                if current_write_id == id {
                     return None;
                 }
-                let Lock::Write(id) = core::mem::replace(self, Lock::Read(vec![id])) else {
+                let Lock::Write(id) =
+                    core::mem::replace(self, Lock::Read(vec![id], Some(current_write_id)))
+                else {
                     unreachable!("LockKind::Write is guaranteed by match");
                 };
                 Some(id)
@@ -35,7 +38,7 @@ impl<Id: TransactionId> Lock<Id> {
     /// Returns the ids of transactions blocking the added write.
     pub fn add_write(&mut self, id: Id) -> Option<Vec<Id>> {
         match core::mem::replace(self, Lock::Write(id)) {
-            Lock::Read(ids) => Some(ids),
+            Lock::Read(ids, _) => Some(ids),
             Lock::Write(current_write_id) => {
                 if current_write_id == id {
                     None
